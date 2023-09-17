c09b5b11485c6cfc451426c6cd605346d762eccb
Du5ker <examerpley@gmail.com>
2023-09-17 21:17:49 +0300

    versionThree

--- knopka/Form1.Designer.cs
+++ knopka/Form1.Designer.cs
@@ -71,6 +71,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -83,6 +84,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.button3_Click);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- knopka/Form1.cs
+++ knopka/Form1.cs
@@ -72,5 +72,35 @@ namespace PhotoViewerApp
                 MessageBox.Show("Ошибка при загрузке изображения: " + ex.Message);
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Получаем путь к папке, в которой находится исполняемый файл приложения
+            string executablePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Складываем путь к изображению относительно этой папки
+            string imagePathRelativeToExecutable = Path.Combine("Images", "busick.jpg");
+
+            // Полный путь к изображению
+            string imagePath = Path.Combine(executablePath, imagePathRelativeToExecutable);
+
+            try
+            {
+                // Проверяем, что файл существует
+                if (File.Exists(imagePath))
+                {
+                    // Загрузка изображения и установка в PictureBox
+                    pictureBox1.Image = Image.FromFile(imagePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл изображения не найден.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке изображения: " + ex.Message);
+            }
+        }
     }
 }
