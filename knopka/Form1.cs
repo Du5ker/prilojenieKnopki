05d784318c7b90d68135da588c3d557cda2b2c11
Du5ker <examerpley@gmail.com>
2023-09-17 20:57:21 +0300

    versionOne

---
+++ knopka/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace PhotoViewerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получаем путь к папке, в которой находится исполняемый файл приложения
+            string executablePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Складываем путь к изображению относительно этой папки
+            string imagePathRelativeToExecutable = Path.Combine("Images", "image.jpg");
+
+            // Полный путь к изображению
+            string imagePath = Path.Combine(executablePath, imagePathRelativeToExecutable);
+
+            try
+            {
+                // Проверяем, что файл существует
+                if (File.Exists(imagePath))
+                {
+                    // Загрузка изображения и установка в PictureBox
+                    pictureBox1.Image = Image.FromFile(imagePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл изображения не найден.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке изображения: " + ex.Message);
+            }
+        }
+    }
+}
